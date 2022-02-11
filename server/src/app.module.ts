1507a9532be1cf53f63c02a20aeeeb71e7d0cbd5
Artur Kotanidi <kotalex2014@gmail.com>
2022-02-11 12:57:26 +0400

    created API server

---
+++ server/src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './modules/users/users.module';
+import { TasksModule } from './modules/tasks/tasks.module';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://kotalex2014:XWfs1DbEVaR0s3aB@cluster0.h61f0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    ),
+    UsersModule,
+    TasksModule,
+  ],
+})
+export class AppModule {}
--- src/app/core/constants/roles.ts
+++
@@ -1,9 +0,0 @@
-const Roles = {
-  ADMIN: "admin",
-  USER: "user"
-};
-
-export const AdminRoles = {roles: [Roles.ADMIN]};
-export const UserRoles = {roles: [Roles.USER]};
-
-export default Roles;
