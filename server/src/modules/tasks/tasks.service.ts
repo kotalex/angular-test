447d9a4a8fd8df9c2c3d5c83349575b469f10e27
Artur Kotanidi <kotalex2014@gmail.com>
2022-02-12 13:51:00 +0400

    removed dist from ignore

--- server/src/modules/tasks/tasks.service.ts
+++ server/src/modules/tasks/tasks.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model, Mongoose, Types } from 'mongoose';
+import { Model, Mongoose } from 'mongoose';
 import { UsersService } from '../users/users.service';
 import { CreateUpdateTaskDto } from './dto/create-task.dto';
 import { Task, TaskDocument } from './schemas/task.schema';
